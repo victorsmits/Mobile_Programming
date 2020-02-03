18d1ce5b2868e819280ea4fdcaa0bcaa3f433e01
Smits <16107@ecam.be>
2020-02-03 19:49:21 +0100

    init

---
+++ Calculator/app/src/main/java/com/example/curencyconverter/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.curencyconverter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultView;
+    Boolean status = false;
+    TextView opeView;
+    List<String> operation = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultView = (TextView) findViewById(R.id.result);
+        opeView = (TextView) findViewById(R.id.ope);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linear_layout_tags);
+
+        Button equalsBtn = new Button(this);
+        equalsBtn.setTag("=");
+        equalsBtn.setText("=");
+        equalsBtn.setLayoutParams(new LinearLayoutCompat.LayoutParams(
+                LinearLayoutCompat.LayoutParams.WRAP_CONTENT,
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT,1));
+
+        equalsBtn.setOnClickListener(new View.OnClickListener()  {
+            public void onClick(View v) {
+                MainActivity.this.equals(v);
+            }
+        });
+
+        layout.addView(equalsBtn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void myClickHandler(View view) {
+        if (this.status) {
+            this.opeView.setText("");
+            this.status = false;
+        }
+        System.out.println(view.getTag().toString());
+        this.operation.add(view.getTag().toString());
+        this.opeView.setText(String.format("%s%s", this.opeView.getText(), view.getTag().toString()));
+    }
+
+    public void equals(View view){
+        List<String> ope = new ArrayList<String>(Arrays.asList("+",
+                "-",
+                "*","-","/"));
+        if(this.opeView.length() < 3 || ope.contains(this.operation.get(2))){
+            Toast.makeText(this, "Please enter a valid operation",
+                    Toast.LENGTH_LONG).show();
+            this.opeView.setText("");
+            this.operation.clear();
+            return;
+        }
+
+        int term1 = Integer.valueOf(this.operation.get(0));
+        int term2 = Integer.valueOf(this.operation.get(2));
+        int result = 0;
+
+
+        switch (this.operation.get(1)){
+            case "+":result = term1 + term2; break;
+            case "-":result = term1 - term2; break;
+            case "*":result = term1 * term2; break;
+            case "/":result = term1 / term2; break;
+        }
+
+        this.resultView.setText(String.valueOf(result));
+        this.operation.clear();
+        this.status = true;
+    }
+
+}
---
+++ CurencyConverter/app/src/main/java/com/example/curencyconverter/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.curencyconverter;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.editText1);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void myClickHandler(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                RadioButton EuroButton = (RadioButton) findViewById(R.id.radio);
+                RadioButton dollarButton = (RadioButton) findViewById(R.id.radio1);
+                if (text.getText().length() == 0) {
+                    Toast.makeText(this, "Please enter a valid number",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                float inputValue = Float.parseFloat(text.getText().toString());
+                if (EuroButton.isChecked()) {
+                    text.setText(String
+                            .valueOf(convertDollarToEuro(inputValue)));
+                    EuroButton.setChecked(false);
+                    dollarButton.setChecked(true);
+                } else {
+                    text.setText(String
+                            .valueOf(convertEuroToDollar(inputValue)));
+                    dollarButton.setChecked(false);
+                    EuroButton.setChecked(true);
+                }
+                break;
+        }
+    }
+    // Convertir Dollar à Euro
+    private float convertDollarToEuro(float dollar) {
+        return (float) (dollar * 0.9);
+    }
+    // Convertir Euro à Dollar
+    private float convertEuroToDollar(float euro) {
+        return (float) (euro / 0.9); // formule à utiliser
+    }
+}
